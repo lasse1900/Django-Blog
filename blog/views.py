64113e75e0cffc069c8978fbf05d1ae3c341382b
Lauri <lauri.kyttala@gmail.com>
2023-11-01 09:45:22 +0200

    Initial commit - basic functionality working

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+posts = [ 
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2023'
+    },
+        {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2023'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
